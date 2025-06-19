7381338fbbde6c6b57dc55604152ccfc1b3d43ee
Gwin14 <fabiospinheira@gmail.com>
2025-06-19 12:03:46 -0400

    feat: users table frontend

--- backend/src/main/java/com/pizzaria/backend/controller/UserController.java
+++ backend/src/main/java/com/pizzaria/backend/controller/UserController.java
@@ -2,8 +2,9 @@ package com.pizzaria.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping; // Import adicionado
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pizzaria.backend.model.User;
@@ -12,6 +13,7 @@ import com.pizzaria.backend.repository.UserRepository;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
     @Autowired
